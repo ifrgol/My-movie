de9839f545af580d8cbf190f42cfa6218d360275
ifrgol <62297013+ifrgol@users.noreply.github.com>
2020-06-15 13:45:08 +0430

    Models class & utils

---
+++ src/main/java/app/my/movie/model/Movie.java
@@ -0,0 +1,57 @@
+package app.my.movie.model;
+
+public class Movie {
+    private int ID;
+    private String title,poster,overview,rate;
+
+    public Movie(int ID, String title, String poster, String overview, String rate) {
+        this.ID = ID;
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+        this.rate = rate;
+    }
+
+    public Movie() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+}
---
+++ src/main/java/app/my/movie/util/Utils.java
@@ -0,0 +1,10 @@
+package app.my.movie.util;
+
+ public class Utils {
+
+ static String key = "6a2f9c8359403a3d7ea9bd8d9a2a46db";
+ static String Popular_Movies = "https://api.themoviedb.org/3/movie/popular?api_key=";
+ static String Lang = "&language=en-US";
+ static String Page = "&page=";
+ static String Image_W500 = "https://image.tmdb.org/t/p/w500/";
+ }
